26fb070e53ddee5ba1ba575d000621d15a18b153
Gopal Sharma <gopal131072@gmail.com>
2017-06-15 22:44:37 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Fetch From URL
+
+Fetches source code from any selected url.
+
+![Here's a gif!](http://imgur.com/a/6qUaY)
---
+++ spec/fetch-from-url-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import FetchFromUrlView from '../lib/fetch-from-url-view';
+
+describe('FetchFromUrlView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
